2da3a28e043c78198e90e16479e1d2321dc5edc1
REghZy <32274404+AngryCarrot789@users.noreply.github.com>
2024-01-19 12:30:38 +0000

    Dragging clips is fully functional, and cursor previews work

--- FramePFX/Editors/Controls/TimelineClipControl.cs
+++ FramePFX/Editors/Controls/TimelineClipControl.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Converters;
 using System.Windows.Threading;
 using FramePFX.Editors.Timelines;
 using FramePFX.Editors.Timelines.Tracks.Clips;
 
 namespace FramePFX.Editors.Controls {
     public class TimelineClipControl : FrameworkElement {
+        private const double MinDragInitPx = 5d;
+        private const double EdgeGripSize = 8d;
+        private const double HeaderGripSize = 20;
+
         private long frameBegin;
         private long frameDuration;
 
@@ -44,10 +51,8 @@ namespace FramePFX.Editors.Controls {
 
         private DragState dragState;
         private Point clickPoint;
+        private Point lastMousePos;
         private bool isUpdatingFrameSpanFromDrag;
-        private long accumulator;
-        private long accumulatorL;
-        private long accumulatorR;
 
         public TimelineClipControl(Clip clip) {
             this.VerticalAlignment = VerticalAlignment.Stretch;
@@ -88,20 +93,72 @@ namespace FramePFX.Editors.Controls {
         protected override void OnMouseDown(MouseButtonEventArgs e) {
             base.OnMouseDown(e);
             this.clickPoint = e.GetPosition(this);
-            this.dragState = DragState.Initiated;
+            this.SetDragState(DragState.Initiated);
             if (!this.IsMouseCaptured)
                 this.CaptureMouse();
         }
 
         protected override void OnMouseUp(MouseButtonEventArgs e) {
             base.OnMouseUp(e);
-            this.dragState = DragState.None;
+            this.SetDragState(DragState.None);
+            this.SetCursorForMousePoint(e.GetPosition(this));
             this.ReleaseMouseCapture();
+        }
+
+        private void SetDragState(DragState state) {
+            if (this.dragState == state) {
+                return;
+            }
+
+            this.dragState = state;
+            this.SetCursorForDragState(state, false);
+        }
+
+        private void SetCursorForDragState(DragState state, bool isPreview) {
+            if (isPreview && this.dragState != DragState.None) {
+                return;
+            }
+
+            switch (state) {
+                case DragState.None:
+                    this.ClearValue(CursorProperty);
+                    break;
+                case DragState.Initiated:
+                    break;
+                case DragState.DragBody:
+                    this.Cursor = Cursors.SizeAll;
+                    break;
+                case DragState.DragLeftEdge:
+                    this.Cursor = Cursors.SizeWE;
+                    break;
+                case DragState.DragRightEdge:
+                    this.Cursor = Cursors.SizeWE;
+                    break;
+                default: throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private void SetCursorForMousePoint(Point mpos) {
+            Size renderSize = this.RenderSize;
+            if (mpos.X <= EdgeGripSize) {
+                this.SetCursorForDragState(DragState.DragLeftEdge, true);
+            }
+            else if (mpos.X >= (renderSize.Width - EdgeGripSize)) {
+                this.SetCursorForDragState(DragState.DragRightEdge, true);
+            }
+            else if (mpos.Y <= HeaderGripSize) {
+                this.SetCursorForDragState(DragState.DragBody, true);
+            }
+            else {
+                this.SetCursorForDragState(DragState.None, true);
+            }
         }
 
         protected override void OnMouseMove(MouseEventArgs e) {
             base.OnMouseMove(e);
             if (this.isUpdatingFrameSpanFromDrag) {
+                // prevent possible stack overflow exceptions, at the cost of the UI possibly glitching a bit.
+                // In my testing, this case is never reached, so it would require something very weird to happen
                 return;
             }
 
@@ -110,143 +167,145 @@ namespace FramePFX.Editors.Controls {
                 return;
             }
 
+            Point mpos = e.GetPosition(this);
+
             if (e.LeftButton != MouseButtonState.Pressed) {
-                this.dragState = DragState.None;
+                this.SetDragState(DragState.None);
+                this.SetCursorForMousePoint(mpos);
                 this.ReleaseMouseCapture();
                 return;
             }
 
-            Point mpos = e.GetPosition(this);
+            this.SetCursorForMousePoint(mpos);
+            TimelineControl timelineCtrl;
+            if (this.Track == null || (timelineCtrl = this.Track.Timeline) == null) {
+                return;
+            }
+
             if (this.dragState == DragState.Initiated) {
-                if (Math.Abs(mpos.X - this.clickPoint.X) < 4d && Math.Abs(mpos.Y - this.clickPoint.Y) < 4d) {
+                if (Math.Abs(mpos.X - this.clickPoint.X) < MinDragInitPx && Math.Abs(mpos.Y - this.clickPoint.Y) < MinDragInitPx) {
                     return;
                 }
 
-                if (this.clickPoint.X <= 4d) {
-                    this.dragState = DragState.DragLeftEdge;
+                if (this.clickPoint.X <= EdgeGripSize) {
+                    this.SetDragState(DragState.DragLeftEdge);
                 }
-                else if (this.clickPoint.X >= (this.ActualWidth - 4d)) {
-                    this.dragState = DragState.DragRightEdge;
+                else if (this.clickPoint.X >= (this.ActualWidth - EdgeGripSize)) {
+                    this.SetDragState(DragState.DragRightEdge);
                 }
-                else {
-                    this.dragState = DragState.DragBody;
+                else if (this.clickPoint.Y <= HeaderGripSize) {
+                    this.SetDragState(DragState.DragBody);
                 }
-
-                this.accumulator = 0;
             }
 
-            if (this.dragState == DragState.DragBody) {
-                TimelineControl timelineCtrl = this.Track?.Timeline;
-                Vector diff = mpos - this.clickPoint;
-                if (Math.Abs(diff.X) >= 1.0d) {
-                    double zoom = timelineCtrl?.UnitZoom ?? 1d;
-                    long offset = (long) Math.Round(diff.X / zoom);
-                    if (offset != 0) {
-                        FrameSpan currSpan = this.Model.Span;
-                        if ((currSpan.Begin + offset) < 0) {
-                            offset = -currSpan.Begin;
-                        }
-
+            if (this.dragState != DragState.None) {
+                double zoom = timelineCtrl.UnitZoom;
+                Vector mdif = mpos - this.clickPoint;
+                FrameSpan oldSpan = this.Model.Span;
+                if (this.dragState == DragState.DragBody) {
+                    if (Math.Abs(mdif.X) >= 1.0d) {
+                        long offset = (long) Math.Round(mdif.X / zoom);
                         if (offset != 0) {
-                            long newBegin = (currSpan.Begin + offset) + this.accumulator;
-                            this.accumulator = 0;
-                            if (newBegin < 0) {
-                                this.accumulator = -newBegin;
-                                newBegin = 0;
-                            }
-
-
-                            FrameSpan newSpan = new FrameSpan(newBegin, currSpan.Duration);
-                            long newEndIndex = newSpan.EndIndex;
-                            if (timelineCtrl != null && newEndIndex > timelineCtrl.Timeline.TotalFrames) {
-                                timelineCtrl.Timeline.TotalFrames = newEndIndex + 300;
-                            }
-
-                            this.isUpdatingFrameSpanFromDrag = true;
-                            this.Model.Span = newSpan;
-                            this.isUpdatingFrameSpanFromDrag = false;
-                        }
-                    }
-                }
-
-                if (Math.Abs(diff.Y) >= 1.0d && timelineCtrl?.Timeline is Timeline timeline) {
-                    int trackIndex = timeline.Tracks.IndexOf(this.Model.Track);
-                    const double area = 0;
-                    if (mpos.Y < Math.Min(area, this.clickPoint.Y)) {
-                        if (trackIndex < 1) {
-                            return;
-                        }
-
-                        this.IsMovingBetweenTracks = true;
-                        this.Model.MoveToTrack(timeline.Tracks[trackIndex - 1]);
-                    }
-                    else if (mpos.Y > (this.ActualHeight - area)) {
-                        if (trackIndex >= (timeline.Tracks.Count - 1)) {
-                            return;
-                        }
-
-                        this.IsMovingBetweenTracks = true;
-                        this.Model.MoveToTrack(timeline.Tracks[trackIndex + 1]);
-                    }
-                }
-            }
-            else if (this.dragState == DragState.DragLeftEdge || this.dragState == DragState.DragRightEdge) {
-                TimelineControl timelineCtrl = this.Track?.Timeline;
-                Vector diff = mpos - this.clickPoint;
-                if (Math.Abs(diff.X) >= 1.0d) {
-                    double zoom = timelineCtrl?.UnitZoom ?? 1d;
-                    long offset = (long) Math.Round(diff.X / zoom);
-                    if (offset != 0) {
-                        FrameSpan currSpan = this.Model.Span;
-                        if (this.dragState == DragState.DragRightEdge) {
-                            long newEndIndex = currSpan.EndIndex + offset + this.accumulatorR;
-                            this.accumulatorR = 0;
-                            if (newEndIndex <= currSpan.Begin) {
-                                this.accumulatorR += (currSpan.Begin - newEndIndex);
-                                newEndIndex = currSpan.Begin + 1;
-                            }
-
-                            FrameSpan newSpan = FrameSpan.FromIndex(currSpan.Begin, newEndIndex);
-                            if (timelineCtrl != null && newEndIndex > timelineCtrl.Timeline.TotalFrames) {
-                                timelineCtrl.Timeline.TotalFrames = newEndIndex + 300;
-                            }
-
-                            this.isUpdatingFrameSpanFromDrag = true;
-                            this.Model.Span = newSpan;
-                            this.isUpdatingFrameSpanFromDrag = false;
-                            this.clickPoint = mpos;
-                            // Debug.WriteLine($"Old = {currSpan}, New = {newSpan}, Offset = {diff}");
-                        }
-                        else {
-                            if ((currSpan.Begin + offset) < 0) {
-                                offset = -currSpan.Begin;
+                            // If begin is 2 and offset is -5, this sets offset to -2
+                            // and since newBegin = begin+offset (2 + -2)
+                            // this ensures begin never drops below 0
+                            if ((oldSpan.Begin + offset) < 0) {
+                                offset = -oldSpan.Begin;
                             }
 
                             if (offset != 0) {
-                                long currEndIndex = currSpan.EndIndex;
-                                long newBegin = (currSpan.Begin + offset) + this.accumulator;
-                                this.accumulator = 0;
-                                if (newBegin < 0) {
-                                    this.accumulator = -newBegin;
-                                    newBegin = 0;
-                                }
-
-                                newBegin += this.accumulatorL;
-                                this.accumulatorL = 0;
-                                if (newBegin >= currEndIndex) {
-                                    this.accumulatorL -= (newBegin - currEndIndex);
-                                    newBegin = currEndIndex - 1;
-                                }
-
-                                FrameSpan newSpan = FrameSpan.FromIndex(newBegin, currEndIndex);
+                                FrameSpan newSpan = new FrameSpan(oldSpan.Begin + offset, oldSpan.Duration);
                                 long newEndIndex = newSpan.EndIndex;
-                                if (timelineCtrl != null && newEndIndex > timelineCtrl.Timeline.TotalFrames) {
+                                if (newEndIndex > timelineCtrl.Timeline.TotalFrames) {
                                     timelineCtrl.Timeline.TotalFrames = newEndIndex + 300;
                                 }
 
                                 this.isUpdatingFrameSpanFromDrag = true;
                                 this.Model.Span = newSpan;
                                 this.isUpdatingFrameSpanFromDrag = false;
+                            }
+                        }
+                    }
+
+                    if (Math.Abs(mdif.Y) >= 1.0d && timelineCtrl.Timeline is Timeline timeline) {
+                        int trackIndex = timeline.Tracks.IndexOf(this.Model.Track);
+                        const double area = 0;
+                        if (mpos.Y < Math.Min(area, this.clickPoint.Y)) {
+                            if (trackIndex < 1) {
+                                return;
+                            }
+
+                            this.IsMovingBetweenTracks = true;
+                            this.Model.MoveToTrack(timeline.Tracks[trackIndex - 1]);
+                        }
+                        else if (mpos.Y > (this.ActualHeight - area)) {
+                            if (trackIndex >= (timeline.Tracks.Count - 1)) {
+                                return;
+                            }
+
+                            this.IsMovingBetweenTracks = true;
+                            this.Model.MoveToTrack(timeline.Tracks[trackIndex + 1]);
+                        }
+                    }
+                }
+                else if (this.dragState == DragState.DragLeftEdge || this.dragState == DragState.DragRightEdge) {
+                    if (Math.Abs(mdif.X) >= 1.0d) {
+                        long offset = (long) Math.Round(mdif.X / zoom);
+                        if (offset != 0) {
+                            if (this.dragState == DragState.DragRightEdge) {
+                                // Clamps the offset to ensure we don't end up with a negative duration
+                                if ((oldSpan.EndIndex + offset) < oldSpan.Begin) {
+                                    // add 1 to ensure clip is always 1 frame long, just because ;)
+                                    offset = -oldSpan.Duration + 1;
+                                }
+
+                                if (offset != 0) {
+                                    long newEndIndex = oldSpan.EndIndex + offset;
+                                    // Clamp new frame span to 1 frame, in case user resizes too much to the right
+                                    // if (newEndIndex >= oldSpan.EndIndex) {
+                                    //     this.dragAccumulator -= (newEndIndex - oldSpan.EndIndex);
+                                    //     newEndIndex = oldSpan.EndIndex - 1;
+                                    // }
+
+                                    FrameSpan newSpan = FrameSpan.FromIndex(oldSpan.Begin, newEndIndex);
+                                    if (newEndIndex > timelineCtrl.Timeline.TotalFrames) {
+                                        timelineCtrl.Timeline.TotalFrames = newEndIndex + 300;
+                                    }
+
+                                    this.isUpdatingFrameSpanFromDrag = true;
+                                    this.Model.Span = newSpan;
+                                    this.isUpdatingFrameSpanFromDrag = false;
+
+                                    // account for there being no "grip" control aligned to the right side;
+                                    // since the clip is resized, the origin point will not work correctly and
+                                    // results in an exponential endIndex increase unless the below code is used.
+                                    // This code is not needed for the left grip because it just naturally isn't
+                                    this.clickPoint.X += (newSpan.EndIndex - oldSpan.EndIndex) * zoom;
+                                }
+                            }
+                            else {
+                                if ((oldSpan.Begin + offset) < 0) {
+                                    offset = -oldSpan.Begin;
+                                }
+
+                                if (offset != 0) {
+                                    long newBegin = oldSpan.Begin + offset;
+                                    // Clamps the offset to ensure we don't end up with a negative duration
+                                    if (newBegin >= oldSpan.EndIndex) {
+                                        // subtract 1 to ensure clip is always 1 frame long
+                                        newBegin = oldSpan.EndIndex - 1;
+                                    }
+
+                                    FrameSpan newSpan = FrameSpan.FromIndex(newBegin, oldSpan.EndIndex);
+                                    long newEndIndex = newSpan.EndIndex;
+                                    if (newEndIndex > timelineCtrl.Timeline.TotalFrames) {
+                                        timelineCtrl.Timeline.TotalFrames = newEndIndex + 300;
+                                    }
+
+                                    this.isUpdatingFrameSpanFromDrag = true;
+                                    this.Model.Span = newSpan;
+                                    this.isUpdatingFrameSpanFromDrag = false;
+                                }
                             }
                         }
                     }
@@ -269,7 +328,7 @@ namespace FramePFX.Editors.Controls {
             Rect rect = new Rect(new Point(), this.RenderSize);
             dc.DrawRectangle(SystemColors.ControlDarkBrush, null, rect);
             if (this.Track?.ClipHeaderColour is Brush headerBrush) {
-                dc.DrawRectangle(headerBrush, null, new Rect(0, 0, rect.Width, Math.Min(rect.Height, 20d)));
+                dc.DrawRectangle(headerBrush, null, new Rect(0, 0, rect.Width, Math.Min(rect.Height, HeaderGripSize)));
             }
 
             // Pen pen = new Pen(Brushes.Black, 1d);
